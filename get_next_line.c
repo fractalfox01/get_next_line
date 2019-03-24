6b46a683a53b2fb2a7c74bc7ddf3a19c0452a620
Thomas Vandivier <tvandivi@e1z2r5p2.42.us.org>
2019-03-24 13:46:36 -0700

    adding working code, removing gt.c, binary, objectfile, etc..

--- get_next_line.c
+++ get_next_line.c
@@ -5,59 +5,95 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tvandivi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/26 23:05:09 by tvandivi          #+#    #+#             */
-/*   Updated: 2019/03/03 21:00:31 by tvandivi         ###   ########.fr       */
+/*   Created: 2019/03/22 13:21:39 by tvandivi          #+#    #+#             */
+/*   Updated: 2019/03/24 12:57:30 by tvandivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "libft/libft.h"
 #include "get_next_line.h"
+#include <stdio.h>
 
-int	get_next_line(const int fd, char **line)
+int	null_check(char **line, char **tab)
 {
-	int			ret;
-	static char *tab[4096];
-	char		*new_line;
-	int			start;
-	int			end;
-	int	i;
-	static int	word_count;
-	int			flag;
-
-	start = 0;
-	end = 0;
-	ret = 0;
-	flag = 1;
-	i = 0;
-	if (!fd)
-		return (0);
-	if (*line)
+	if (ft_strlen(*tab) > 0)
 	{
-		tab[fd] = ft_strnew(BUFF_SIZE + 1);
-		if ((ret = read(fd, tab[fd], BUFF_SIZE)) > 0)
-		{
-			while (i < (BUFF_SIZE * (word_count + 1)))
-			{
-				if (tab[fd][i] == '\n')
-				{
-					end = i;
-					word_count++;
-					tab[fd][i] = '\0';
-					ft_memcpy(*line, tab[fd], (end));
-					break ;
-				}
-			//	printf("tab[%d][%d] = %c\n", fd, i, tab[fd][i]);
-				i++;
-			}	
-			if (ret == BUFF_SIZE || tab[fd][end] == '\n')
-			{
-				return (1);
-			}
-			return (0);
-		}
+		ft_memdel((void **)line);
+		*line = ft_strjoin(*tab, "");
+		ft_bzero(*tab, ft_strlen(*tab));
+		return (1);
+	}
+	return (0);
+}
+
+int	read_next_buffer(int a, char **tab, char **line, char *buf)
+{
+	int	j;
+
+	j = 0;
+	buf[a] = '\0';
+	while (buf[j] != '\n' && j < (a - 1))
+		j++;
+	if (buf[j] == '\n')
+	{
+		buf[j] = '\0';
+		ft_memdel((void **)line);
+		*line = ft_strjoin(*tab, buf);
+		ft_bzero(*tab, ft_strlen(*tab));
+		if (j >= (a - 1))
+			*tab = ft_strdup("");
+		else
+			*tab = ft_strdup(buf + (j + 1));
+		return (1);
+	}
+	return (0);
+}
+
+int	chomp_line(char **tab, char **line)
+{
+	char	*tmp;
+
+	if (!((ft_strchr(*tab, '\n')) == NULL))
+	{
+		ft_memdel((void **)line);
+		tmp = ft_strchr(*tab, '\n');
+		*tmp = '\0';
+		*line = ft_strdup(*tab);
+		tmp++;
+		ft_bzero(*tab, ft_strlen(*tab));
+		*tab = ft_strjoin(*tab, tmp);
+		return (1);
+	}
+	return (0);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	*tab[FD_LIMIT];
+	char		*buf;
+	int			a;
+	int			j;
+
+	a = 0;
+	j = 0;
+	if (!(tab[fd]))
+		tab[fd] = ft_strdup("");
+	buf = ft_strnew(BUFF_SIZE + 1);
+	if (ft_strlen(tab[fd]) > 0)
+		if (chomp_line(&tab[fd], line) == 1)
+			return (1);
+	while ((a = read(fd, buf, BUFF_SIZE)) >= 0)
+	{
+		if (a == 0)
+			return (null_check(line, &tab[fd]));
+		if ((read_next_buffer(a, &tab[fd], line, buf)) == 1)
+			return (1);
+		tab[fd] = ft_strjoin(tab[fd], buf);
+		ft_memset(buf, '\0', BUFF_SIZE);
+		free(buf);
+		buf = ft_strnew(BUFF_SIZE + 1);
 	}
 	return (0);
 }
