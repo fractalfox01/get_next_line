a86d3813088f44b04b3fd7c3a36bb8b602aa6482
Thomas Vandivier <tvandivi@e1z3r2p17.42.us.org>
2019-03-30 12:39:28 -0700

    updating - more testing: All looks well

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: tvandivi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 18:39:57 by tvandivi          #+#    #+#             */
-/*   Updated: 2019/03/28 13:00:31 by tvandivi         ###   ########.fr       */
+/*   Updated: 2019/03/30 12:38:01 by tvandivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE	42
+# define BUFF_SIZE	9
 # define FD_LIMIT	4891
 
 int	get_next_line(const int fd, char **line);
